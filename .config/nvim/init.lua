d53dd8e303865b532b6cb05aa05d725e0af91581
smplcd <asmplcd@gmail.com>
2023-07-28 21:25:45 +0000

    Add nvim plugins
    Packer is the plugin manager recommended by ThePrimeagen. I'm inclined
    to use it, simply because it manages itself and has support for lua
    plugins. Ezpz.
    
    Add 3 keymaps for the telescope 'fzf'

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,9 +7,28 @@ vim.opt.softtabstop = 4
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 
+vim.g.mapleader = " "
 
 vim.keymap.set("i", "{", "{<Enter>}<Esc>O")
 vim.keymap.set("i", "printf", "printf(\"\");<Esc>hhi")
 
 vim.keymap.set("n", "<C-j>", "<C-d>")
 vim.keymap.set("n", "<C-k>", "<C-u>")
+
+require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.2',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+end)
+
+-- telecope
+local telecmd = require('telescope.builtin')
+
+vim.keymap.set('n', '<leader>ff', telecmd.find_files, {}) -- find files
+vim.keymap.set('n', '<leader>fg', telecmd.git_files, {}) -- find git
+vim.keymap.set('n', '<leader>fs', telecmd.live_grep, {}) -- find string (require ripgrep found in edge/community which fucks with my env)
+
