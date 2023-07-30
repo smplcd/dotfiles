af9988ee84cfb50e4efa788cbe6133c1136b75ed
smplcd <asmplcd@gmail.com>
2023-07-30 18:22:50 +0000

    Clangd doesn't work on OpenBSD (by default)
    I'm sure I could thinker a solution, but I really just want to work. I
    used to be able to code without autocompletion before, this is far from
    a necessity.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -23,21 +23,6 @@ require('packer').startup(function(use)
 		'nvim-telescope/telescope.nvim', tag = '0.1.2',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		requires = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},             -- Required
-			{'williamboman/mason.nvim'},           -- Optional
-			{'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},     -- Required
-			{'hrsh7th/cmp-nvim-lsp'}, -- Required
-			{'L3MON4D3/LuaSnip'},     -- Required
-		},
-}
 end)
 
 -- telecope
@@ -46,57 +31,3 @@ local telecmd = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', telecmd.find_files, {}) -- find files
 vim.keymap.set('n', '<leader>fg', telecmd.git_files, {}) -- find git
 vim.keymap.set('n', '<leader>fs', telecmd.live_grep, {}) -- find string (require ripgrep found in edge/community which fucks with my env)
-
--- lsp-zero
-local lsp = require("lsp-zero")
-
-lsp.preset("recommended")
-
-lsp.ensure_installed({})
-
--- Fix Undefined global 'vim'
-lsp.nvim_workspace()
-
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-
-lsp.setup_nvim_cmp({
-	lsp.defaults.cmp_mappings({
-		['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-		['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-		['<C-i>'] = cmp.mapping.confirm({ select = true }),
-		["<C-Space>"] = cmp.mapping.complete(),
-		['<Tab>'] = nil,
-		['<S-Tab>'] = nil,
-	})
-})
-
-lsp.set_preferences({
-    sign_icons = {
-		error = 'E',
-		warn  = 'W',
-		hint  = 'H',
-		info  = 'I',
-    }
-})
-
-lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
-
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-end)
-
-lsp.setup()
-
-vim.diagnostic.config({
-    virtual_text = true
-})
